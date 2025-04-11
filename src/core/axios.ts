71d3b282b310a3f32937924c0b4e1e65c9cabac5
har9421 <har9421@users.noreply.github.com>
2025-04-11 14:13:14 +0530

    updated config

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const apiClient = axios.create({
-  baseURL: process.env.AUTHSERVICE_URL, 
+  baseURL: process.env.REACT_APP_AUTHSERVICE_URL, 
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
